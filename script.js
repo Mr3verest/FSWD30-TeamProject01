48161e39d1dd2f81b9ae08bfb182390e26467992
PascalTonsern <pascal.tonsern@gmail.com>
2017-12-12 11:24:54 +0100

    adds first drafts

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById("home").addEventListener("click", function(){
+	window.location.href = "index.html";
+});
+
+document.getElementById("member1").addEventListener("click", function(){
+	window.location.href = "team-member.html";
+});
+document.getElementById("member2").addEventListener("click", function(){
+	window.location.href = "team-member.html";
+});
+document.getElementById("member3").addEventListener("click", function(){
+	window.location.href = "team-member.html";
+});
+document.getElementById("member4").addEventListener("click", function(){
+	window.location.href = "team-member.html";
+});
+
