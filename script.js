fd738f59a23d0dc305a64b62e56fb1dab31bf3f1
PascalTonsern <pascal.tonsern@gmail.com>
2017-12-13 09:53:28 +0100

    updates files, adds member pages

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ document.getElementById("home").addEventListener("click", function(){
 });
 
 document.getElementById("member1").addEventListener("click", function(){
-	window.location.href = "team-member.html";
+	window.location.href = "member-Pascal.html";
 });
 document.getElementById("member2").addEventListener("click", function(){
-	window.location.href = "team-member.html";
+	window.location.href = "member-Sade.html";
 });
 document.getElementById("member3").addEventListener("click", function(){
-	window.location.href = "team-member.html";
+	window.location.href = "member-Mahtab.html";
 });
 document.getElementById("member4").addEventListener("click", function(){
-	window.location.href = "team-member.html";
+	window.location.href = "member-Ibrahim.html";
 });
 
