0d0fd2dd6cd2bb4682a501ee4f75b8a9ba18aab5
PascalTonsern <pascal.tonsern@gmail.com>
2017-12-14 10:49:49 +0100

    finished?

--- script.js
+++ script.js
@@ -31,6 +31,6 @@ function showSlides() {
     if (slideIndex > slides.length) {slideIndex = 1}    
     
     slides[slideIndex-1].style.display = "block";  
-    setTimeout(showSlides, 8000); // Change image every 8 seconds
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
 // ______________ Background Carousell END_____________
